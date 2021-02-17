362e3e3fedb6f4bf2e61403f324bebcfa564d0c1
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-18 02:43:51 +0530

    Credit Card Generation without Luhn Algo

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,1 +1,47 @@
 # Write your code here
+import secrets
+import string
+
+accounts = {}
+while 1:
+    choice = input("""1. Create an account
+2. Log into account
+0. Exit
+""")
+    print()
+    if choice == "0":
+        print("Bye!")
+        break
+    elif choice == "1":
+        credit_card_num = "400000" + "".join(secrets.choice(string.digits) for _ in range(10))
+        pin = "".join(secrets.choice(string.digits) for _ in range(4))
+        bal = 0
+        print("""Your card has been created
+Your card number:
+{}
+Your card PIN:
+{}\n""".format(credit_card_num, pin))
+        accounts[credit_card_num] = {"pin": pin, "balance": bal}
+    elif choice == "2":
+        cc_num = input("Enter your card number:\n")
+        peen = input("Enter your PIN:\n")
+        print()
+        if cc_num not in accounts.keys() or peen != accounts[cc_num]["pin"]:
+            print("Wrong card number or PIN:\n")
+            continue
+        print('You have successfully logged in!\n')
+        while 1:
+            acc_choice = input("""1. Balance
+2. Log out
+0. Exit
+""")
+            print()
+            if acc_choice == "0":
+                break
+            elif acc_choice == "1":
+                print("Balance: {}\n".format(bal))
+            elif acc_choice == "2":
+                print("You have successfully logged out!\n")
+                break
+
+
